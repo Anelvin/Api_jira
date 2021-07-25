1d0c3c8453ddb3cbc454a2641557f89c95016bf8
Anelvin <amejiaroque@gmail.com>
2021-07-25 11:20:57 +0200

    Database connected

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from 'express';
-
+// import db from './models/index.js';
+import morgan from 'morgan';
 const app = express();
 
+import databse from './database/db.js';
+
 app.set('port', process.env.PORT || 3001);
+
+// app.use(db);
 
 app.get('/', (req, res, next) => {
     return res.send('API JIRA');
