54b41cfaa0d58a010970a31725b45ba41ee0b48d
Anelvin <amejiaroque@gmail.com>
2021-07-29 00:49:43 +0200

    Return token on method register

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,6 @@
 import { User } from '../database/db';
 import { StatusCodes } from 'http-status-codes';
-import { createUser, signIn } from '../services/authService';
+import { signUp, signIn } from '../services/authService';
 
 const AuthController = {};
 
@@ -10,8 +10,12 @@ AuthController.signIn = async (req, res, next) => {
 };
 
 AuthController.signUp = async (req, res, next) => {
-    const user = await createUser(req.body);
-    return res.status(StatusCodes.CREATED).json({'message':'User created!', data: user});
+    const user = await signUp(req.body);
+    try {
+        return res.status(StatusCodes.CREATED).json({'message':'User created!', data: user});
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+    }
 };
 
 export default AuthController;
